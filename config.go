273c1cfe15e2f2f04eafcdd0904245740d73fe45
Pete Heist <pete@heistp.net>
2024-05-26 09:59:17 +0200

    re-organize config

--- config.go
+++ config.go
@@ -7,8 +7,71 @@ import (
 	"time"
 )
 
+//
+// Common Settings
+//
+
 // Sender: test duration
 const Duration = 20 * time.Second
+
+// IFace: rate and rate schedule
+var Rate = 1 * Mbps
+var RateSchedule = []RateAt{
+	//RateAt{Clock(10 * time.Second), 200 * Mbps},
+}
+
+// Iface: AQM config
+// var UseAQM = NewDelmin1(Clock(5 * time.Millisecond))
+var UseAQM = NewDelmin2(Clock(25000*time.Microsecond),
+	Clock(500*time.Microsecond))
+
+// var UseAQM = NewRamp()
+var SCERampMin = Clock(TransferTime(Rate, Bytes(MSS))) * 1
+
+const SCERampMax = Clock(100 * time.Millisecond)
+
+// Sender: flows
+var Flows = []Flow{
+	AddFlow(SCE, true),
+	//AddFlow(SCE, true),
+}
+var FlowSchedule = []FlowAt{
+	//FlowAt{1, Clock(10 * time.Second), false},
+}
+
+// Delay: path delays for each flow
+var FlowDelay = []Clock{
+	Clock(20 * time.Millisecond),
+	Clock(20 * time.Millisecond),
+	Clock(20 * time.Millisecond),
+	Clock(20 * time.Millisecond),
+}
+
+//
+// Plots
+//
+
+// Iface: plots
+const (
+	PlotSojourn = true
+	PlotMarks   = false
+)
+
+// Sender: plots
+const (
+	PlotInFlight = false
+	PlotCwnd     = true
+	PlotRTT      = false
+)
+
+// Receiver: plots
+const (
+	PlotGoodput = true
+)
+
+//
+// Less Common Settings
+//
 
 // Sender: SCE-MD params
 const (
@@ -21,58 +84,6 @@ const (
 	MSS      = 1500
 	IW       = 10 * MSS
 	RTTAlpha = float64(0.1)
-)
-
-// Sender: plots
-const (
-	PlotInFlight = false
-	PlotCwnd     = true
-	PlotRTT      = false
-)
-
-// Sender: flows
-var Flows = []Flow{
-	AddFlow(SCE, true),
-	AddFlow(SCE, false),
-}
-
-var FlowSchedule = []FlowAt{
-	FlowAt{1, Clock(10 * time.Second), true},
-}
-
-// Iface: plots
-const (
-	PlotSojourn = true
-	PlotMarks   = false
-)
-
-// IFace: rate and rate schedule
-var Rate = 100 * Mbps
-var RateSchedule = []RateAt{
-	//RateAt{Clock(10 * time.Second), 200 * Mbps},
-}
-
-// Iface: AQM config
-// var UseAQM = NewDelmin1(Clock(5 * time.Millisecond))
-var UseAQM = NewDelmin2(Clock(5000*time.Microsecond),
-	Clock(100*time.Microsecond))
-
-// var UseAQM = NewRamp()
-var SCERampMin = Clock(TransferTime(Rate, Bytes(MSS))) * 1
-
-const SCERampMax = Clock(100 * time.Millisecond)
-
-// Delay: delays for each flow
-var FlowDelay = []Clock{
-	Clock(20 * time.Millisecond),
-	Clock(20 * time.Millisecond),
-	Clock(20 * time.Millisecond),
-	Clock(20 * time.Millisecond),
-}
-
-// Receiver
-const (
-	PlotGoodput = true
 )
 
 // main
